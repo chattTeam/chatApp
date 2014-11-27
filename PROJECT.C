18558f30a1b3f2a9ba1c9c7f81279f0da3233e3c
Marwa Ibraheem <marwa>
2014-11-27 18:48:15 +0200

    linked

--- PROJECT.C
+++ PROJECT.C
@@ -1,9 +1,134 @@
 #include <stdio.h>
 #include <conio.h>
 
+struct phonebook {
+
+	char name [80];
+	char phone[20];
+	char address[30];
+	struct phonebook * next ;
+	struct phonebook * prev;
+};
+
+struct phonebook * head=NULL;
+struct phonebook * tail=NULL;
+
+struct phonebook * create_phonebook (void);
+void append_phonebook (struct phonebook * ele);
+void  search_phonebook (char * name);
+void delete_phonebook (char * key) ;
+void deletelist(void);
+void printlist_search (void) ;
+
 int main (char ** argv, int argc)
 {
 	clrscr();
 	getch();
 	return 0;
 }
+
+
+struct phonebook * create_phonebook (void)
+
+{
+
+		return ((struct phonebook *) malloc (sizeof(struct phonebook)));
+
+}
+
+void append_phonebook (struct phonebook * ele)
+{
+	if(head == NULL)
+	{
+		head=tail=ele;
+		ele -> prev =NULL;
+		ele -> next =NULL;
+
+	}
+	else
+	{
+		tail -> next=ele;
+		ele -> prev=tail;
+		tail=ele;
+		ele ->next= NULL;
+
+	}
+
+}
+void search_phonebook (char * name)
+{
+	struct phonebook * temp=head;
+
+	while (temp!=NULL && strcmp(temp -> name,name) )
+	{
+		 temp=temp -> next;
+	}
+
+	   return temp;
+}
+
+void deletelist(void)
+{
+	struct phonebook * temp;
+	while(head != NULL)
+		{
+			temp=head;
+			head=head -> next;
+			free(temp);
+		}
+
+	   tail=NULL;
+}
+void delete_phonebook (char * key)
+{
+	struct phonebook * temp;
+	temp=search_phonebook (key);
+
+	if(temp != NULL)
+	{
+		if(temp == head && head ==tail)
+		{
+			head=tail=NULL;
+			free(temp);
+		}
+
+		if(head == temp && head != tail)
+		{
+			head=head -> next;
+			head -> prev =NULL;
+			free(temp);
+		}
+
+		if(tail == temp && head != tail)
+		{
+			   tail = tail -> prev;
+			   tail -> next =NULL;
+			   free(temp);
+
+		}
+
+		else
+		{
+			temp -> prev -> next =temp -> next;
+			temp -> next -> prev =temp -> prev;
+			free(temp);
+
+
+		}
+	}
+}
+
+void printlist_search (void)
+{
+	struct phonebook * ptr = head;
+
+	printf("\n the list is : \n");
+	while(ptr != NULL)
+	{
+		 printf("\n %d \n",ptr->code);
+		 ptr=ptr->next;
+
+	}
+
+}
+
